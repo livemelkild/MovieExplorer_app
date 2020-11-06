1c50003db4459374d3fa7c7bf7948c621c71b463
Live Pettersdatter Melkild <livepm@stud.ntnu.no>
2020-11-06 13:14:37 +0100

    live

---
+++ MovieApp/App.tsx
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/movie`)
+      .then(res => res.json()) //format the resault to json
+      .then(res => {
+          console.log(res)
+          setItems(res.DATA)
+          });},[]);
+
+  return (
+    <div>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      {items?.map((item:any) => (
+          <div key={item._id}>
+            film tittel: {item.title}
+          </div>
+      ))
+      }
+      <StatusBar style="auto" />
+    </View>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
