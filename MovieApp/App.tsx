cf526757bb11a7afde951174188d733f4cf7eff2
Anne Høgdahl Skjærseth <annehsk@stud.ntnu.no>
2020-11-10 20:02:41 +0100

    searchbar work

--- MovieApp/App.tsx
+++ MovieApp/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MovieIterator from "./Components/MovieIterator";
+import SearchBar from "./Components/SearchBar";
+import { Provider } from "react-redux"
+import Store from "./Store"
+import { Header } from 'react-native-elements';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Provider store={Store}>
+      <Header 
+        centerComponent={{text: "MovieExplorer"}}
+      />
+
+      <SearchBar/>
       <MovieIterator />
-    </View>
+    </Provider>
   );
 }
 
